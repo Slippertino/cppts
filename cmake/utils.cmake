cf53dd53305657b9952218091178dbad48744581
Скулков Илья Русланович <iliya-skulkov@mail.ru>
2023-04-07 17:12:29 +0300

    in utils.cmake relative cmake path edited

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -23,7 +23,7 @@ function(win_copy_deps_to_target_dir target)
     if(CMAKE_MAJOR_VERSION GREATER 3 OR CMAKE_MINOR_VERSION GREATER_EQUAL 21)
         set(has_runtime_dll_genex YES)
         add_custom_command(TARGET ${target} POST_BUILD
-            COMMAND ${CMAKE_COMMAND} -P "${CMAKE_SOURCE_DIR}/cmake/silent_copy.cmake"
+            COMMAND ${CMAKE_COMMAND} -P "${CMAKE_CURRENT_SOURCE_DIR}/../cmake/silent_copy.cmake"
                 "$<TARGET_RUNTIME_DLLS:${target}>" "$<TARGET_FILE_DIR:${target}>"
             COMMAND_EXPAND_LISTS)
     endif()
@@ -34,12 +34,12 @@ function(win_copy_deps_to_target_dir target)
         if(dep_type STREQUAL "SHARED_LIBRARY")
             if(NOT has_runtime_dll_genex)
                 add_custom_command(TARGET ${target} POST_BUILD
-                    COMMAND ${CMAKE_COMMAND} -P "${CMAKE_SOURCE_DIR}/cmake/silent_copy.cmake" 
+                    COMMAND ${CMAKE_COMMAND} -P "${CMAKE_CURRENT_SOURCE_DIR}/../cmake/silent_copy.cmake" 
                         "$<TARGET_FILE:${dep}>" "$<TARGET_PDB_FILE:${dep}>" "$<TARGET_FILE_DIR:${target}>"
                     COMMAND_EXPAND_LISTS)
             else()
                 add_custom_command(TARGET ${target} POST_BUILD
-                    COMMAND ${CMAKE_COMMAND} -P "${CMAKE_SOURCE_DIR}/cmake/silent_copy.cmake"
+                    COMMAND ${CMAKE_COMMAND} -P "${CMAKE_CURRENT_SOURCE_DIR}/../cmake/silent_copy.cmake"
                         "$<TARGET_PDB_FILE:${dep}>" "$<TARGET_FILE_DIR:${target}>"
                     COMMAND_EXPAND_LISTS)
             endif()
